3ab9577d629945c39e8dff920dd07517f79e291e
TeuTeuf <antoine.teuf@gmail.com>
2019-02-05 23:15:09 +0100

    Correct code format corresponding to lint rules

--- web/js/client.js
+++ web/js/client.js
@@ -1,28 +1,25 @@
+/* global $ */
+/* global TrelloPowerUp */
+
 const { Promise } = TrelloPowerUp;
 
 const HLTB_ICON = 'https://howlongtobeat.com/img/icons/favicon-96x96.png';
 
-const getBadges = function (t) {
-  return t.card('name')
-    .get('name')
-    .then((cardName) => {
-    return [{
-      dynamic: () => new Promise((resolve) => {
-        $.get('/hltb/' + cardName, (hltbResult) => {
-          resolve({
-            title: 'HOW LONG TO BEAT',
-            text: hltbResult.duration,
-            icon: HLTB_ICON,
-            color: hltbResult.color
-          });
+const getBadges = t => t.card('name')
+  .get('name')
+  .then(cardName => [{
+    dynamic: () => new Promise((resolve) => {
+      $.get(`/hltb/${cardName}`, (hltbResult) => {
+        resolve({
+          title: 'HOW LONG TO BEAT',
+          text: hltbResult.duration,
+          icon: HLTB_ICON,
+          color: hltbResult.color,
         });
-      })
-    }];
-  });
-};
+      });
+    }),
+  }]);
 
 TrelloPowerUp.initialize({
-  'card-badges': function (t, options) {
-    return getBadges(t);
-  },
+  'card-badges': t => getBadges(t),
 });

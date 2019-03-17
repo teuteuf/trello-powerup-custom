2e6efc8ab2fbcf4e88477a460280a461d9e618aa
TeuTeuf <antoine.teuf@gmail.com>
2019-03-17 23:32:09 +0100

    Only display HLTB badge if card has Game label

--- web/js/client.js
+++ web/js/client.js
@@ -5,20 +5,25 @@ const { Promise } = TrelloPowerUp;
 
 const HLTB_ICON = 'https://howlongtobeat.com/img/icons/favicon-96x96.png';
 
-const getBadges = t => t.card('name')
-  .get('name')
-  .then(cardName => [{
-    dynamic: () => new Promise((resolve) => {
-      $.get(`/hltb/${cardName}`, (hltbResult) => {
-        resolve({
-          title: 'HOW LONG TO BEAT',
-          text: hltbResult.duration,
-          icon: HLTB_ICON,
-          color: hltbResult.color,
+const getBadges = t => t.card('name', 'labels')
+  .then((cardInfo) => {
+    const labelGame = cardInfo.labels.find(label => label.name === 'Game' || label.name === 'Jeu');
+    if (!labelGame) {
+      return [];
+    }
+    return [{
+      dynamic: () => new Promise((resolve) => {
+        $.get(`/hltb/${cardInfo.name}`, (hltbResult) => {
+          resolve({
+            title: 'HOW LONG TO BEAT',
+            text: hltbResult.duration,
+            icon: HLTB_ICON,
+            color: hltbResult.color,
+          });
         });
-      });
-    }),
-  }]);
+      }),
+    }];
+  });
 
 TrelloPowerUp.initialize({
   'card-badges': t => getBadges(t),
